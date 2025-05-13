778030bd87af5a942e42610a13731748fde006b7
Gautam-2604 <gaurisariag@gmail.com>
2025-05-13 18:34:20 +0530

    some changes

--- api-server/index.js
+++ api-server/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const { generateSlug } = require('random-word-slugs')
 const { ECSClient, RunTaskCommand } = require('@aws-sdk/client-ecs')
 const { Server } = require('socket.io')
+const Redis = require('ioredis')
 
 const app = express()
 const PORT = 9000
 
+const subscriber = new Redis('')
 
 const io = new Server({ cors: '*' })
 
@@ -68,5 +70,16 @@ app.post('/project', async (req, res) => {
     return res.json({ status: 'queued', data: { projectSlug, url: `http://${projectSlug}.localhost:8000` } })
 
 })
+
+async function initRedisSubscribe() {
+    console.log('Subscribed to logs....')
+    subscriber.psubscribe('logs:*')
+    subscriber.on('pmessage', (pattern, channel, message) => {
+        io.to(channel).emit('message', message)
+    })
+}
+
+
+initRedisSubscribe()
 
 app.listen(PORT, () => console.log(`API Server Running..${PORT}`))
--- build-server/script.js
+++ build-server/script.js
@@ -1,58 +1,74 @@
-const {exec} = require('child_process');
-const fs = require('fs');
-const path = require('path');
-const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3')
-const mime = require('mime-types');
+const { exec } = require('child_process')
+const path = require('path')
+const fs = require('fs')
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
+const mime = require('mime-types')
+const Redis = require('ioredis')
+
+
+const publisher = new Redis('')
+
 
 const s3Client = new S3Client({
-    region: 'ap-south-1',
+    region: '',
     credentials: {
-        accessKeyId: 'AKIA4HWJUDI6CWEMQQ6X',
-        secretAccessKey: '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'
+        accessKeyId: '',
+        secretAccessKey: ''
     }
 })
 
-const PROJECT_ID = process.env.PROJECT_ID;
+const PROJECT_ID = process.env.PROJECT_ID
 
-async function init(){
-    console.log('Executing Script...');
-    const outDirPath = path.join(__dirname, 'output');
+
+function publishLog(log) {
+    publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }))
+}
+
+async function init() {
+    console.log('Executing script.js')
+    publishLog('Build Started...')
+    const outDirPath = path.join(__dirname, 'output')
+
     const p = exec(`cd ${outDirPath} && npm install && npm run build`)
 
-    p.stdout.on('data', (data) => {
-        console.log(data.toString());
+    p.stdout.on('data', function (data) {
+        console.log(data.toString())
+        publishLog(data.toString())
     })
-    
-    p.stdout.on('error', (error) => {
-        console.error('Error:', error);
-    })
-    p.on('close',async ()=>{
-        console.log('Script execution completed.');
-        const distFolderPath = path.join(outDirPath, 'output','dist');
-        const distFolderContent = fs.readdirSync(distFolderPath,{recursive:true});
 
-        for (const file of distFolderContent){
+    p.stdout.on('error', function (data) {
+        console.log('Error', data.toString())
+        publishLog(`error: ${data.toString()}`)
+    })
+
+    p.on('close', async function () {
+        console.log('Build Complete')
+        publishLog(`Build Complete`)
+        const distFolderPath = path.join(__dirname, 'output', 'dist')
+        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true })
+
+        publishLog(`Starting to upload`)
+        for (const file of distFolderContents) {
             const filePath = path.join(distFolderPath, file)
             if (fs.lstatSync(filePath).isDirectory()) continue;
 
-            console.log('Uploading file path:', filePath);
-            
+            console.log('uploading', filePath)
+            publishLog(`uploading ${file}`)
 
             const command = new PutObjectCommand({
-                Bucket: 'vercel-but-better',
+                Bucket: 'vercel-clone-outputs',
                 Key: `__outputs/${PROJECT_ID}/${file}`,
                 Body: fs.createReadStream(filePath),
-                ContentType: mime.lookup(filePath) ,
-        })
+                ContentType: mime.lookup(filePath)
+            })
 
-        await s3Client.send(command)
-        //https://github.com/Gautam-2604/vercel_test_proj
-    }
-
-
-        
+            await s3Client.send(command)
+            publishLog(`uploaded ${file}`)
+            console.log('uploaded', filePath)
+        }
+        publishLog(`Done`)
+        console.log('Done...')
     })
-
-
-    
 }
+
+init()
